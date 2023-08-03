51af448e095d21278b07131798e226cc2d9eaf41
unknown <haymeemotoh22@gmail.com>
2023-08-03 16:38:53 +0800

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["winter"],
+  }
+} satisfies Config;
